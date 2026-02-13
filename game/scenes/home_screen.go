4e84576d1f213d0098075faf57e9a38b34e68137
cauaof_souza <cauasantos064@gmail.com>
2026-02-13 02:34:58 -0300

    Adiciona imagem à column
    - Agora todo Draw e Update é chamado a partir da Column, pois todos componentes da scene estão encapsulados nela
    - Só há Column nos fields da scene

--- game/scenes/home_screen.go
+++ game/scenes/home_screen.go
@@ -1,81 +1,34 @@
 package scenes
 
 import (
-	"image/color"
 	"log"
 
 	"github.com/allanjose001/go-battleship/game/components"
 	"github.com/allanjose001/go-battleship/game/components/basic"
+	"github.com/allanjose001/go-battleship/game/components/basic/colors"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type HomeScreen struct {
-	layout       components.Widget
-	homeImage    *ebiten.Image
-	alturaDoTopo float32
-	sairDoJogo   bool
+	layout     components.Widget //deixei tudo no layout
+	sairDoJogo bool
 }
 
+func (m *HomeScreen) OnExit(_ Scene) {}
+
 func (m *HomeScreen) OnEnter(_ Scene, screenSize basic.Size) {
-	var err error
-	m.homeImage, _, err = ebitenutil.NewImageFromFile("assets/images/home-screen.png")
+	err := m.init(screenSize)
 
 	if err != nil {
-		log.Fatal("Erro ao carregar a imagem:", err)
+		log.Fatal("Erro ao carregar componentes na tela inicial: ", err)
 	}
 
-	m.alturaDoTopo = 450.0
-
-	m.layout = components.NewColumn(
-		basic.Point{X: 0, Y: m.alturaDoTopo},
-		20,
-		basic.Size{W: screenSize.W, H: screenSize.H - m.alturaDoTopo},
-		basic.Start,
-		basic.Center,
-		[]components.Widget{
-			components.NewButton(
-				basic.Point{},
-				basic.Size{W: 300, H: 50},
-				"Jogar",
-				color.RGBA{R: 48, G: 67, B: 103, A: 255},
-				nil,
-				func(bt *components.Button) {
-					log.Println("Botão clicado!") // Aqui ficará a função que inicia o jogo (mudar para a tela de jogo)
-				},
-			),
-
-			components.NewButton(
-				basic.Point{},
-				basic.Size{W: 300, H: 50},
-				"Ranking",
-				color.RGBA{R: 48, G: 67, B: 103, A: 255},
-				nil,
-				func(bt *components.Button) {
-					log.Println("Botão clicado!") // Aqui ficará a função que mostra o ranking (mudar para a tela de ranking)
-				},
-			),
-
-			components.NewButton(
-				basic.Point{},
-				basic.Size{W: 300, H: 50},
-				"Sair",
-				color.RGBA{R: 48, G: 67, B: 103, A: 255},
-				nil,
-				func(bt *components.Button) {
-					m.sairDoJogo = true
-					log.Println("Saindo do jogo...")
-				},
-			),
-		},
-	)
 }
 
 func (m *HomeScreen) Update() error {
 	if m.layout != nil {
 		m.layout.Update(basic.Point{X: 0, Y: 0})
 	}
-
 	if m.sairDoJogo {
 		return ebiten.Termination
 	}
@@ -84,28 +37,64 @@ func (m *HomeScreen) Update() error {
 }
 
 func (m *HomeScreen) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{R: 13, G: 27, B: 42, A: 255})
-
-	if m.homeImage != nil {
-		escalaImagem := 0.8
-
-		larguraTela := screen.Bounds().Dx()
-		larguraImagem := m.homeImage.Bounds().Dx()
-		larguraImagemEscalada := float64(larguraImagem) * escalaImagem
-
-		posicaoX := float64((larguraTela - int(larguraImagemEscalada)) / 2)
-
-		posicaoY := 60.0
-
-		opts := &ebiten.DrawImageOptions{}
-		opts.GeoM.Scale(escalaImagem, escalaImagem)
-		opts.GeoM.Translate(posicaoX, posicaoY)
-		screen.DrawImage(m.homeImage, opts)
-	}
-
 	if m.layout != nil {
 		m.layout.Draw(screen)
 	}
 }
 
-func (m *HomeScreen) OnExit(_ Scene) {}
+// init Inicializa componentes
+func (m *HomeScreen) init(screenSize basic.Size) error {
+	var err error
+	homeImage, err := components.NewImage(
+		"assets/images/home-screen.png",
+		basic.Point{},
+		basic.Size{W: 580, H: 400})
+
+	if err != nil {
+		return err
+	}
+	m.layout = components.NewColumn(
+		basic.Point{},
+		20,
+		basic.Size{W: screenSize.W, H: screenSize.H},
+		basic.Center,
+		basic.Center,
+		[]components.Widget{
+			homeImage,
+			components.NewButton(
+				basic.Point{},
+				basic.Size{W: 400, H: 50},
+				"Jogar",
+				colors.Dark,
+				nil,
+				func(bt *components.Button) {
+					log.Println("Botão clicado!") // Aqui ficará a função que inicia o jogo (mudar para a tela de jogo)
+				},
+			),
+
+			components.NewButton(
+				basic.Point{},
+				basic.Size{W: 400, H: 50},
+				"Ranking",
+				colors.Dark,
+				nil,
+				func(bt *components.Button) {
+					log.Println("Botão clicado!") // Aqui ficará a função que mostra o ranking (mudar para a tela de ranking)
+				},
+			),
+
+			components.NewButton(
+				basic.Point{},
+				basic.Size{W: 400, H: 50},
+				"Sair",
+				colors.Dark,
+				nil,
+				func(bt *components.Button) {
+					m.sairDoJogo = true
+					log.Println("Saindo do jogo...")
+				},
+			),
+		},
+	)
+	return nil
+}
